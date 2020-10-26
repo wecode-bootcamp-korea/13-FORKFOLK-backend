719fc8237a12159c9728ec364ae39b06b572eee5
Hyoniii <phhphh841@hanmail.net>
2020-10-26 22:15:22 +0900

    Add : product/views ShopDetailView 작성

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,8 @@
 from django.urls   import path
 
-from product.views import ShopAllView 
+from product.views import ShopAllView,ShopDetailView 
 
 urlpatterns = [
-    path('',ShopAllView.as_view())
+    path('',ShopAllView.as_view()),
+    path('/<int:product_id>',ShopDetailView.as_view())
     ]
--- product/views.py
+++ product/views.py
@@ -1,4 +1,5 @@
 import json
+import random
 
 from django.db.models          import Q
 from django.views              import View
@@ -29,6 +30,7 @@ class ShopAllView(View):
         if category_name == "All":
             products      = Product.objects.all()
 
+
             for product in products:
                 product_id = product.id
                 name       = product.name
@@ -40,6 +42,7 @@ class ShopAllView(View):
                 product_dic["name"]     = name
                 product_dic["category"] = category.name
                 product_dic["price"]    = price
+                product_dic["image"]    = ProductImage.objects.filter(product_id=product_id).values('image_url')[0].get("image_url")
 
                 product_list.append(product_dic)
             page      = page_num
@@ -65,7 +68,42 @@ class ShopAllView(View):
                 product_dic["name"]     = name
                 product_dic["category"] = category.name
                 product_dic["price"]    = price
+                product_dic["image"]    = ProductImage.objects.filter(product_id=product_id).values('image_url')[0].get("image_url")
+
 
                 product_list.append(product_dic)
 
             return JsonResponse({"category_products":product_list},status=201)
+
+class ShopDetailView(View):
+    def get(self,request,product_id):
+        product = Product.objects.get(id=product_id)
+
+        related_from = product.category_id
+        related_to = Product.objects.filter(category_id=related_from)
+        image = ProductImage.objects.filter(product_id=product.id).values("image_url")[0].get("image_url")
+        #print(image)
+        related_list = []
+
+        detail_product   = {
+            "id"          : product.id,
+            "name"        : product.name,
+            "price"       : product.price,
+            "description" : product.description,
+            "shipping"    : product.shipping,
+            "image"       : 
+            [image for image in ProductImage.objects.filter(product_id=product.id).values('image_url')]
+        }
+
+        for product in related_to:
+            related_product            = {}
+            related_product["id"]      = product.id
+            related_product["name"]    = product.name
+            related_product["image"]   = image
+            related_product["categoy"] = ShopCategory.objects.get(id=product.category_id).name
+            related_product["price"]   = product.price
+
+            related_list.append(related_product)
+
+        random_list = random.sample(related_list,6)
+        return JsonResponse({'product_info':detail_product,'related_product':random_list},status=201)
