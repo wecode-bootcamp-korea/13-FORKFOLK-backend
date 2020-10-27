26909c18cb1309c85449be4e9f20748f1758eb06
Hyoniii <phhphh841@hanmail.net>
2020-10-27 10:14:52 +0900

    Modify : product/views 예외처리

--- product/views.py
+++ product/views.py
@@ -20,90 +20,103 @@ from product.models   import (
 
 class ShopAllView(View):
     def get(self,request):
-        product_list  = []
-        category_list = []
-        category_name = request.GET.get("category",None)
+        try: 
+            product_list  = []
+            category_list = []
+            category_name = request.GET.get("category",None)
 
-        if request.GET.get("page"):
-            page_num = int(request.GET.get("page"))
+            if request.GET.get("page"):
+                page_num = int(request.GET.get("page"))
 
-        if category_name == "All":
-            products      = Product.objects.all()
+            if category_name == "All":
+                products      = Product.objects.all()
 
+                for product in products:
+                    product_id = product.id
+                    name       = product.name
+                    price      = product.price
+                    category   = ShopCategory.objects.get(id=product.category_id)
 
-            for product in products:
-                product_id = product.id
-                name       = product.name
-                price      = product.price
-                category   = ShopCategory.objects.get(id=product.category_id)
+                    product_dic             = {}
+                    product_dic["id"]       = product_id
+                    product_dic["name"]     = name
+                    product_dic["category"] = category.name
+                    product_dic["price"]    = price
+                    product_dic["image"]    = ProductImage.objects.filter(product_id=product_id).values('image_url')[0].get("image_url")
 
-                product_dic             = {}
-                product_dic["id"]       = product_id
-                product_dic["name"]     = name
-                product_dic["category"] = category.name
-                product_dic["price"]    = price
-                product_dic["image"]    = ProductImage.objects.filter(product_id=product_id).values('image_url')[0].get("image_url")
+                    product_list.append(product_dic)
+                page      = page_num
+                page_size = 12
+                limit     = page * page_size
+                offset    = limit - page_size
+                
+                return JsonResponse({"page_products":product_list[offset:limit]}, status=201)
 
-                product_list.append(product_dic)
-            page      = page_num
-            page_size = 12
-            limit     = page * page_size
-            offset    = limit - page_size
-            return JsonResponse({"page_products":product_list[offset:limit]}, status=201)
+            else:
+                category_id   = ShopCategory.objects.get(name=category_name)
+                products = Product.objects.filter(category_id=category_id)
 
+                for product in products:
+                    product_id = product.id
+                    name       = product.name
+                    price      = product.price
+                    category   = ShopCategory.objects.get(id=product.category_id)
 
+                    product_dic             = {}
+                    product_dic["id"]       = product_id
+                    product_dic["name"]     = name
+                    product_dic["category"] = category.name
+                    product_dic["price"]    = price
+                    product_dic["image"]    = ProductImage.objects.filter(product_id=product_id).values('image_url')[0].get("image_url")
 
-        else:
-            category_id   = ShopCategory.objects.get(name=category_name)
-            products = Product.objects.filter(category_id=category_id)
+                    product_list.append(product_dic)
 
-            for product in products:
-                product_id = product.id
-                name       = product.name
-                price      = product.price
-                category   = ShopCategory.objects.get(id=product.category_id)
+                return JsonResponse({"category_products":product_list},status=201)
 
-                product_dic             = {}
-                product_dic["id"]       = product_id
-                product_dic["name"]     = name
-                product_dic["category"] = category.name
-                product_dic["price"]    = price
-                product_dic["image"]    = ProductImage.objects.filter(product_id=product_id).values('image_url')[0].get("image_url")
-
-
-                product_list.append(product_dic)
-
-            return JsonResponse({"category_products":product_list},status=201)
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'},status=400)
+        except ValueError:
+            return JsonResponse({'message':'VALUE_ERROR'},status=400)
+        except TypeError:
+            return JsonResponse({'message':'TYPE_ERROR'},status=400)
 
 class ShopDetailView(View):
     def get(self,request,product_id):
-        product = Product.objects.get(id=product_id)
+        try:
+            product = Product.objects.get(id=product_id)
+            related_from = product.category_id
+            related_to = Product.objects.filter(category_id=related_from)
+            image = ProductImage.objects.filter(product_id=product.id).values("image_url")[0].get("image_url")
+        
+            related_list = []
+            detail_product   = {
+                "id"          : product.id,
+                "name"        : product.name,
+                "price"       : product.price,
+                "description" : product.description,
+                "shipping"    : product.shipping,
+                "image"       : 
+                [image for image in ProductImage.objects.filter(product_id=product.id).values('image_url')]
+            }
 
-        related_from = product.category_id
-        related_to = Product.objects.filter(category_id=related_from)
-        image = ProductImage.objects.filter(product_id=product.id).values("image_url")[0].get("image_url")
-        #print(image)
-        related_list = []
+            for product in related_to:
+                related_product            = {}
+                related_product["id"]      = product.id
+                related_product["name"]    = product.name
+                related_product["image"]   = image
+                related_product["categoy"] = ShopCategory.objects.get(id=product.category_id).name
+                related_product["price"]   = product.price
 
-        detail_product   = {
-            "id"          : product.id,
-            "name"        : product.name,
-            "price"       : product.price,
-            "description" : product.description,
-            "shipping"    : product.shipping,
-            "image"       : 
-            [image for image in ProductImage.objects.filter(product_id=product.id).values('image_url')]
-        }
+                related_list.append(related_product)
 
-        for product in related_to:
-            related_product            = {}
-            related_product["id"]      = product.id
-            related_product["name"]    = product.name
-            related_product["image"]   = image
-            related_product["categoy"] = ShopCategory.objects.get(id=product.category_id).name
-            related_product["price"]   = product.price
+            random_list = random.sample(related_list,6)
+            
+            return JsonResponse({'product_info':detail_product,'related_product':random_list},status=201)
 
-            related_list.append(related_product)
+          except KeyError:
+              return JsonResponse({'message':'KEY_ERROR'},status=400)
+          except ValueError:
+              return JsonResponse({'message':'VALUE_ERROR'},status=400)
+          except TypeError:
+              return JsonResponse({'message':'TYPE_ERROR'},status=400)
 
-        random_list = random.sample(related_list,6)
-        return JsonResponse({'product_info':detail_product,'related_product':random_list},status=201)
